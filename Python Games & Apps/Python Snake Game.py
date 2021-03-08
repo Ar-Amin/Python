ead23ebb40ada346d5575e21c5038347acbfd0cd
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-03-09 00:32:17 +0200

    Add files via upload

--- Python Games & Apps/Python Snake Game.py
+++ Python Games & Apps/Python Snake Game.py
@@ -38,18 +38,23 @@ while True:
     if key == curses.KEY_UP:
         new_head[0] -= 1
     if key == curses.KEY_RIGHT:
-        new_head[0] -= 1
+        new_head[1] += 1
     if key == curses.KEY_LEFT:
-        new_head[0] += 1
+        new_head[1] -= 1
 
     snake.insert(0, new_head)
 
     if snake[0] == food:
         food = None
-    while food is None:
-        nf = [
-            random.randint(1, sh-1),
-            random.randint(1, sw-1)
-        ]
-        food = nf if nf not in snake else None
-w.addch(food[0], food[1], curses.ACS_P)
+        while food is None:
+            nf = [
+                random.randint(1, sh-1),
+                random.randint(1, sw-1)
+            ]
+            food = nf if nf not in snake else None
+        w.addch(food[0], food[1], curses.ACS_PI)
+    else:
+        tail = snake.pop()
+        w.addch(tail[0], tail[1], ' ')
+
+    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
