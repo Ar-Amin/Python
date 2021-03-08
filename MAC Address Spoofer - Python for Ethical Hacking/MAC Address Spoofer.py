946d15a0bc4bd688a758f13caa2ed2a895d06f5a
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-03-08 12:32:17 +0200

    Add files via upload

--- MAC Address Spoofer - Python for Ethical Hacking/MAC Address Spoofer.py
+++ MAC Address Spoofer - Python for Ethical Hacking/MAC Address Spoofer.py
@@ -31,7 +31,7 @@ def mac_spoof(interface, mac_add):
 
 
 def mac_check(interface):
-    # checking ifconfig interface for validity (like lo interface etc...)
+    # checking ifconfig interface for validity (like lo interface etc.....)
     ifconfig = subprocess.check_output(["ifconfig", options.interface])
     current_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
     if current_mac.group(0):
