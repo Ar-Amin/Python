a13d08c956faf38f0c4cbe5b1005a99d970a8d8e
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-03-20 01:40:22 +0200

    Add files via upload

--- Python Games & Apps/youtube-downlader.py
+++ Python Games & Apps/youtube-downlader.py
@@ -5,8 +5,30 @@ link = 'https://www.youtube.com/watch?v=FVq6TYw9WjE&list=PLuXY3ddo_8nzCVqXcTFqwc
 
 video = YouTube(link)
 
-print(f"The video title is:\n {video.title} \n ---------------------")
-print(f"The video description is:\n{video.description} \n ----------------")
-print(f"The video views are: {video.views} \n --------------------")
-print(f"The video rating is: {video.rating} \n ------------------")
-print(f"The video duration is: {video.length} \n-----------------")
+# print(f"The video title is:\n {video.title} \n ---------------------")
+# print(f"The video description is:\n{video.description} \n ----------------")
+# print(f"The video views are: {video.views} \n --------------------")
+# print(f"The video rating is: {video.rating} \n ------------------")
+# print(f"The video duration is: {video.length} \n-----------------")
+
+# print(video.streams)
+
+# for stream in video.streams:
+#     print(stream)
+
+# for stream in video.streams.filter(progressive=True):
+#     print(stream)
+#         or
+# print(video.streams.get_highest_resolution())
+#          or
+# print(video.streams.get_lowest_resolution())
+
+
+def finish():
+    print("dowenload done")
+
+
+video.streams.get_lowest_resolution().download(
+    output_path="/home/neo/Downloads")
+
+video.register_on_complete_callback(finish())
