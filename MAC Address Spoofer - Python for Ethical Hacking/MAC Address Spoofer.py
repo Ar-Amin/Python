7f5746ec276b164b0151b98195b2429a174abbef
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-03-30 22:52:06 +0200

    Add files via upload

--- MAC Address Spoofer - Python for Ethical Hacking/MAC Address Spoofer.py
+++ MAC Address Spoofer - Python for Ethical Hacking/MAC Address Spoofer.py
@@ -4,7 +4,7 @@ import optparse
 
 # Get Options For Program For Interface And The New MAC Address
 # Change the MAC address
-# Check MAC address with new MAC address
+# Check MAC Address With new MAC address
 
 
 def get_options():
@@ -31,7 +31,7 @@ def mac_spoof(interface, mac_add):
 
 
 def mac_check(interface):
-    # Checking ifconfig interface for validity (like lo interface etc......)
+    # Checking ifconfig interface for validity (like lo interface etc.........)
     ifconfig = subprocess.check_output(["ifconfig", options.interface])
     current_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
     if current_mac.group(0):
