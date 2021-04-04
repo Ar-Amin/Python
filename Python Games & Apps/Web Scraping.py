caa888e356ea59c329881899ede7eca2935ca1ae
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-04-04 11:00:45 +0200

    Add files via upload

--- Python Games & Apps/Web Scraping.py
+++ Python Games & Apps/Web Scraping.py
@@ -4,7 +4,7 @@
 # -- pip/pip3 install beautifulsoup4
 
 import requests
-from bs4 import Beaurifulsoup
+from bs4 import BeautifulSoup
 import csv
 from itertools import zip_longest
 
@@ -18,3 +18,14 @@ src = result.content
 print(src)
 
 # 4th step create soup object to parse content
+soup = BeautifulSoup(src, "lxml")
+# 5th step the elements containing info we need
+# -- job titles, jop skills, company names, location names
+
+job_titles = soup.find_all("h2", {"class": "css-m604qf"})
+# print(job_titles)
+comany_name = soup.find_all("a", {"class": "css-17s97q8"})
+location_name = soup.find_all("span", {"class": "css-5wys0k"})
+job_skills = soup.find_all("div", {"class": "css-y4udm8"})
+
+# 6th step loop over returned lists to extract needed info into other lists
