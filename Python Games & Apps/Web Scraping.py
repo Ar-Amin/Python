86e663f7b0e7b1ea54d0267d58cc178752aa2696
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-04-06 15:58:12 +0200

    Add files via upload

--- Python Games & Apps/Web Scraping.py
+++ Python Games & Apps/Web Scraping.py
@@ -8,6 +8,11 @@ from bs4 import BeautifulSoup
 import csv
 from itertools import zip_longest
 
+job_title = []
+comany_name = []
+location_name = []
+skills = []
+
 # 2nd step use requestes to fetch the url
 result = requests.get(
     "https://wuzzuf.net/search/jobs/?a=hpb%7Cspbg&q=network%20administration")
@@ -24,8 +29,22 @@ soup = BeautifulSoup(src, "lxml")
 
 job_titles = soup.find_all("h2", {"class": "css-m604qf"})
 # print(job_titles)
-comany_name = soup.find_all("a", {"class": "css-17s97q8"})
-location_name = soup.find_all("span", {"class": "css-5wys0k"})
+comany_names = soup.find_all("a", {"class": "css-17s97q8"})
+location_names = soup.find_all("span", {"class": "css-5wys0k"})
 job_skills = soup.find_all("div", {"class": "css-y4udm8"})
 
 # 6th step loop over returned lists to extract needed info into other lists
+for i in range(len(job_titles)):
+    job_title.append(job_titles[i].text)
+    comany_name.append(comany_names[i].text)
+    location_name.append(location_names[i].text)
+    skills.append(job_skills[i].text)
+
+
+#  7 th Step creat csv file and fill it with values
+file_list = [job_title, comany_name, location_name, skills]
+exported = zip_longest(*file_list)
+with open("/home/neo/Documents/Python/CodzelaPython/test.csv", "w") as myFile:
+    wr = csv.writer(myFile)
+    wr.writerow(["job title", "company name", "location", "skills"])
+    wr.writerows()
