a8d9c8d87aaec0e18a990a3722640102ad1e45a3
Ar-Amin <69108773+Ar-Amin@users.noreply.github.com>
2021-04-09 18:54:56 +0200

    Add files via upload

--- Python Games & Apps/Guessing.py
+++ Python Games & Apps/Guessing.py
@@ -8,7 +8,7 @@ out_of_guesses = False
 while guess != secret_word and not out_of_guesses:
     if guess_count < guess_limit:
 
-        guess = input("enter guess")
+        guess = input("Enter Guess")
         guess_count += 1
     else:
         out_of_guesses = True
